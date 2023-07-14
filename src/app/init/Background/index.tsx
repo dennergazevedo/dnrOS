b98c06c8406803048e9139aca686853d377b0ecf
Denner Azevedo <denner.gomes@ciahering.com.br>
2023-07-14 15:14:58 -0300

    Init completed

---
+++ src/app/init/Background/index.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+import React from 'react';
+import Bg from '@/assets/bg.png'
+
+const Background: React.FC = () => {
+  return (
+    <Image src={Bg} alt='background' className='flex absolute bottom-0 object-cover left-0 w-screen h-screen' />
+  );
+}
+
+export default Background;
---
+++ src/app/init/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Background from './Background';
+
+const DnrOS: React.FC = () => {
+  return (
+    <section className='flex flex-col w-screen h-screen'>
+      <Background />
+    </section>
+  );
+}
+
+export default DnrOS;
