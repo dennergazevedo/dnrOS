93055a04c2b0abc06f4e8ee98d72396ef421b33d
Denner Azevedo <denner.gomes@ciahering.com.br>
2023-07-18 20:25:55 -0300

    Add GA

---
+++ src/app/head.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Script from 'next/script'
+
+const Head: React.FC = () => {
+  return (
+    <head>
+      <title>dnr.OS</title>
+      <meta name="description" content="Denner Azevedo - Web Developer" />
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-BD497N434V"/>
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-BD497N434V');
+        `}
+      </Script>
+    </head>
+  );
+}
+
+export default Head;
